50168c615399fbeb98ceba4ba5bc1aadecfbea82
mitsuaki kuwahara <quwahara@gmail.com>
2019-05-25 13:09:38 +0900

    Got start Olbi. Add objectAssignDeep

---
+++ olbi.js
@@ -0,0 +1,129 @@
+(function (definition) {
+  if (typeof exports === "object") {
+    // CommonJS
+    module.exports = definition();
+  } else if (typeof define === "function" && define.amd) {
+    // RequireJS
+    define(definition);
+  } else {
+    // script
+    Olbi = definition();
+  }
+})(function () {
+  'use strict';
+  return (function () {
+
+
+    function isArray(v) {
+      return Array.isArray(v);
+    }
+
+    function isObject(v) {
+      return v && !Array.isArray(v) && (typeof v) === "object";
+    }
+
+    function clone(value) {
+
+      if (isObject(value)) {
+        return cloneObject(value);
+      }
+
+      if (isArray(value)) {
+        return cloneArray(value);
+      }
+
+      return value;
+    }
+
+    function cloneArray(array) {
+      return array.map(clone);
+    }
+
+    function cloneObject(object) {
+      var theClone = {};
+      for (var key in object) {
+        if (!Object.prototype.hasOwnProperty.call(object, key)) {
+          continue;
+        }
+        theClone[key] = clone(object[key]);
+      }
+      return theClone;
+    }
+
+    /**
+     * 
+     * @param {Object} target 
+     * @param {Array} sources 
+     * @see https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+     * @see https://github.com/saikojosh/Object-Assign-Deep
+     */
+    function objectAssignDeep(target, sources) {
+      if (target == null) {
+        throw new TypeError('Cannot convert undefined or null to object');
+      }
+
+      var target2 = Object(target);
+
+      for (var i = 1; i < arguments.length; ++i) {
+        var source = arguments[i];
+
+        if (source == null) {
+          continue;
+        }
+
+        for (var key in source) {
+          // Avoid bugs when hasOwnProperty is shadowed
+          if (!Object.prototype.hasOwnProperty.call(source, key)) {
+            continue;
+          }
+
+          var sVal = source[key];
+          var tVal = target2[key];
+          var sValClone;
+
+          if (isObject(sVal)) {
+            sValClone = cloneObject(sVal);
+            if (isObject(tVal)) {
+              target2[key] = objectAssignDeep(tVal, sValClone);
+            } else {
+              target2[key] = sValClone;
+            }
+            continue;
+          }
+
+          if (isArray(sVal)) {
+            sValClone = cloneArray(sVal);
+            if (isArray(tVal)) {
+              target2[key] = tVal.concat(sValClone);
+            } else {
+              target2[key] = sValClone;
+            }
+            continue;
+          }
+
+          target2[key] = sVal;
+        }
+
+      }
+      return target2;
+    }
+
+    var Lbi = function Lbi() {
+
+    };
+
+    var Olbi = function Olbi(structure, opts) {
+    };
+
+    Olbi.prototype = objectAssignDeep({
+    },
+      Lbi.prototype);
+
+    Olbi.prototype.constructor = Olbi;
+
+    Olbi.objectAssignDeep = objectAssignDeep;
+
+    return Olbi;
+  })();
+
+});
